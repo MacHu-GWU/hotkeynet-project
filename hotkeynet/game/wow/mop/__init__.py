2c02eb64f45443ab7bc7bd86ebf8afebd9e8b90e
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-08-25 14:38:30 -0400

    add mop module

---
+++ hotkeynet/game/wow/mop/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import coordinator
+from .model import Character
+from .talent import (
+    Talent,
+    TalentCategory,
+)
+from ..model import Window, Account
---
+++ hotkeynet/game/wow/mop/coordinator.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+"""
+游戏窗口相关的数值.
+
+例如游戏相对于桌面的绝对坐标. 游戏窗口的宽度高度. 以及各种 UI 界面中的按钮的坐标
+相对于游戏窗口内部的相对坐标.
+"""
+
+# 我们有三个分辨率模式, 分别对应高, 中, 低
+window_left_top_x_at_1920_1080 = 120
+window_left_top_y_at_1920_1080 = 0
+
+window_left_top_x_at_1600_900 = 120
+window_left_top_y_at_1600_900 = 0
+
+window_left_top_x_at_1176_664 = 120
+window_left_top_y_at_1176_664 = 0
+
+window_width_at_1920_1080 = 1800
+window_height_at_1920_1080 = 1012
+
+window_width_at_1600_900 = 1610
+window_height_at_1600_900 = 935
+
+window_width_at_1176_664 = 1186
+window_height_at_1176_664 = 699
+
+# 在游戏登录界面当输入了错误的账号密码时弹出的错误窗口上的确认按钮的坐标
+wrong_password_pop_up_x_at_1920_1080 = 890
+wrong_password_pop_up_y_at_1920_1080 = 565
+
+wrong_password_pop_up_x_at_1600_900 = 801
+wrong_password_pop_up_y_at_1600_900 = 518
+
+wrong_password_pop_up_x_at_1176_664 = 585
+wrong_password_pop_up_y_at_1176_664 = 380
+
+# 在游戏登录界面的账号输入框坐标
+username_input_box_x_at_1920_1080 = 900
+username_input_box_y_at_1920_1080 = 505
+
+username_input_box_x_at_1600_900 = 804
+username_input_box_y_at_1600_900 = 471
+
+username_input_box_x_at_1176_664 = 585
+username_input_box_y_at_1176_664 = 347
+
+# 在游戏界面按下 ECS 的弹出菜单中的 登出按钮
+log_out_button_x_at_1920_1080 = 999
+log_out_button_y_at_1920_1080 = 999
+
+log_out_button_x_at_1600_900 = 804
+log_out_button_y_at_1600_900 = 476
+
+log_out_button_x_at_1176_664 = 585
+log_out_button_y_at_1176_664 = 347
+
+# 在游戏界面按下 ECS 的弹出菜单中的 返回按钮
+return_to_game_button_x_at_1920_1080 = 999
+return_to_game_button_y_at_1920_1080 = 999
+
+return_to_game_button_x_at_1600_900 = 801
+return_to_game_button_y_at_1600_900 = 525
+
+return_to_game_button_x_at_1176_664 = 585
+return_to_game_button_y_at_1176_664 = 385
+
+# 用户点击放弃 roll 物品的红叉按钮的坐标, 需要和 domino 动作条插件的设置配合使用
+pass_item_button_x_at_1920_1080 = 1525
+pass_item_button_1_y_at_1920_1080 = 924
+pass_item_button_2_y_at_1920_1080 = 857
+pass_item_button_3_y_at_1920_1080 = 790
+pass_item_button_4_y_at_1920_1080 = 724
+
+pass_item_button_x_at_1600_900 = 1365
+pass_item_button_1_y_at_1600_900 = 852
+pass_item_button_2_y_at_1600_900 = 795
+pass_item_button_3_y_at_1600_900 = 725
+pass_item_button_4_y_at_1600_900 = 668
+
+pass_item_button_x_at_1176_664 = 1005
+pass_item_button_1_y_at_1176_664 = 632
+pass_item_button_2_y_at_1176_664 = 589
+pass_item_button_3_y_at_1176_664 = 542
+pass_item_button_4_y_at_1176_664 = 497
