e1283fc005c32e7ac398df582b9722d51f6a3fd7
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-10-22 11:43:54 -0400

    Update hkn.py

--- hotkeynet/app/warmane/hkn.py
+++ hotkeynet/app/warmane/hkn.py
@@ -1296,7 +1296,7 @@ class HknScript(AttrsClass):
                     x=self.mode.game_client.rdf_confirm_role_accept_button_x,
                     y=self.mode.game_client.rdf_confirm_role_accept_button_y,
                 )
-                hk.Wait.make(milli=500)
+                hk.Wait.make(milli=2000)
                 hk.ClickMouse(button=KN.MOUSE_LButton).set_mode_as_x_y(
                     x=self.mode.game_client.rdf_enter_dungeon_button_x,
                     y=self.mode.game_client.rdf_enter_dungeon_button_y,
