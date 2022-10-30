3ef18c7c75e2e23007e8d4898910f2b3190c32fd
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-10-30 16:08:02 -0400

    c

---
+++ hotkeynet/act_factory.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+"""
+
+
+class ActFactoryMeta(type):
+    """
+    meta class of ActFactory
+    """
+
+    def __new__(cls, name, bases, attrs):
+        for k, v in attrs.items():
+            if not k.startswith("_"):
+                @classmethod
+                def func(cls) -> str:
+                    return v
+
+                attrs[k] = func
+        klass = super(ActFactoryMeta, cls).__new__(cls, name, bases, attrs)
+        return klass
+
+
+class ActFactory(metaclass=ActFactoryMeta):
+    pass
