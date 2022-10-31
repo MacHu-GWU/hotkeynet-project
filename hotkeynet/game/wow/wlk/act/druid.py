8a7803b09cd383bd55c9ebacf0e177dd0bfdc865
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-10-31 09:44:37 -0400

    c

--- hotkeynet/game/wow/wlk/act/druid.py
+++ hotkeynet/game/wow/wlk/act/druid.py
@@ -1,2 +1,88 @@
 # -*- coding: utf-8 -*-
 
+from hotkeynet import ActFactory
+
+
+class Balance(ActFactory):
+    Barkskin = None  #
+    Cyclone = None  #
+    Entangling_Roots = None  #
+    Faerie_Fire = None  #
+    Force_of_Nature = None  #
+    Hibernate = None  #
+    Innervate = None  #
+    Insect_Swarm = None  #
+    Moonfire = None  #
+    Moonkin_FormShapeshift = None  #
+    Nature_s_Grasp = None  #
+    Soothe_Animal = None  #
+    Starfall = None  #
+    Starfire = None  #
+    Teleport_Moonglade = None  #
+    Thorns = None  #
+    Typhoon = None  #
+    Wrath = None  #
+
+
+class Restoration(ActFactory):
+    Abolish_Poison = None  # 驱毒术
+    Cure_Poison = None  # 治疗中毒
+    Gift_of_the_Wild = None  # 野性赐福
+    Healing_Touch = None  # 治疗之触
+    Lifebloom = None  # 生命之花
+    Mark_of_the_Wild = None  # 野性印记
+    Nature_s_Swiftness = None  # 自然迅捷, 下一个技能瞬发, 恢复系天赋技能
+    Nourish = None  # 滋养
+    Rebirth = None  # 战斗复活
+    Regrowth = None  # 愈合
+    Rejuvenation = None  # 回春术
+    Remove_Curse = None  # 驱散诅咒
+    Revive = None  # 复活
+    Swiftmend = None  # 迅捷治愈, 恢复系天赋技能
+    Tranquility = None  # 宁静, 长 CD 小队治疗
+    Tree_of_LifeShapeshift = None  # 治疗之树形态
+    Wild_Growth = None  # 野性生长, 群体治疗, 恢复系天赋技能
+
+
+class Feral(ActFactory):
+    Aquatic_FormShapeshift = None  #
+    Bash = None  #
+    Bear_FormShapeshift = None  #
+    Berserk = None  #
+    Cat_FormShapeshift = None  #
+    Challenging_Roar = None  #
+    Claw = None  #
+    Cower = None  #
+    Dash = None  #
+    Demoralizing_Roar = None  #
+    Dire_Bear_FormShapeshift = None  #
+    Enrage = None  #
+    Faerie_Fire_Feral = None  #
+    Feral_Charge_Bear = None  #
+    Feral_Charge_Cat = None  #
+    Ferocious_Bite = None  #
+    Flight_FormShapeshift = None  #
+    Frenzied_Regeneration = None  #
+    Growl = None  #
+    Lacerate = None  #
+    Leader_of_the_Pack = None  #
+    Maim = None  #
+    Mangle_Bear = None  #
+    Mangle_Cat = None  #
+    Maul = None  #
+    Pounce = None  #
+    Primal_Fury = None  #
+    Prowl = None  #
+    Rake = None  #
+    Ravage = None  #
+    Rip = None  #
+    Savage_DefensePassive = None  #
+    Savage_Roar = None  #
+    Shred = None  #
+    Survival_Instincts = None  #
+    Swift_Flight_FormShapeshift = None  #
+    Swipe_Bear = None  #
+    Swipe_Cat = None  #
+    Tiger_s_Fury = None  #
+    Track_Humanoids = None  #
+    Travel_FormShapeshift = None  #
--- hotkeynet/game/wow/wlk/act/shaman.py
+++ hotkeynet/game/wow/wlk/act/shaman.py
@@ -1,2 +1,111 @@
 # -*- coding: utf-8 -*-
 
+from .....act_factory import ActFactory
+
+class ElementalCombat:
+    # 元素系主打技能
+    Lightning_Bolt = None # 闪电箭
+    Chain_Lightning = None # 闪电链
+    Lava_Burst = None # 熔岩爆发
+    Thunderstorm = None # 雷霆风暴, 元素系天赋技能
+
+    # 震击
+    Flame_Shock = None # 火焰震击
+    Earth_Shock = None # 大地震击
+    Frost_Shock = None # 冰霜震击
+
+    Elemental_Mastery = None # 元素掌握, 增加施法速度, 爆发技能, 元素系天赋技能
+    Fire_Nova = None # 火焰新星, 火系图腾 AOE 伤害
+    Hex = None # 妖术
+    Purge = None # 净化术
+
+    # 图腾
+    Searing_Totem = None # 灼热图腾, 攻击周围敌人
+    Magma_Totem = None  # 熔岩爆裂图腾, 周期性 AOE 伤害
+    Stoneclaw_Totem = None # 石爪图腾, 嘲讽周围敌人保护施法者
+    Earthbind_Totem = None  # 地缚图腾, 进入范围内的敌人减速
+    Wind_Shear = None # 削风术, 打断敌人施法
+    Fire_Elemental_Totem = None # 火元素图腾
+    Totem_of_Wrath = None # 愤怒图腾, 增加周围友军的法伤, 元素系天赋技能
+
+    Call_of_the_Ancestors = None  # 一次性插 4 个图腾, 组合 1
+    Call_of_the_Elements = None  # 一次性插 4 个图腾, 组合 2
+    Call_of_the_Spirits = None  # 一次性插 4 个图腾, 组合 3
+
+
+class Enhancement:
+    # 增强系主打技能
+    Stormstrike = None # 风暴打击, 同时用双手武器攻击, 增强西天赋技能
+    Lava_Lash = None # 熔岩猛击, 用副手武器攻击, 增强系天赋技能
+
+    Bloodlust_or_Heroism = None # 嗜血 / 英勇, 为团队提高急速
+    Astral_Recall = None # 先祖召回, 跟炉石一样
+    Earth_Elemental_Totem = None # 土元素图腾
+    Far_Sight = None # 远视
+    Feral_Spirit = None # 召唤幽灵狼, 增强系天赋技能
+
+    # 抗性图腾
+    Fire_Resistance_Totem = None # 火焰抗性图腾
+    Frost_Resistance_Totem = None # 冰霜抗性图腾
+    Nature_Resistance_Totem = None # 自然抗性图腾
+    Flametongue_Totem = None # 火舌图腾, 为周围的团队成员增加法伤
+    Flametongue_Weapon = None # 火舌武器, 增加法伤, 攻击时带火焰伤害
+    Rockbiter_Weapon = None # 石化武器, 提高武器伤害
+    Frostbrand_Weapon = None # 冰霜武器, 攻击时带减移动速度
+    Lightning_Shield = None # 闪电盾
+    Ghost_Wolf = None # 变形幽灵狼
+    Grounding_Totem = None # 根基图腾, 吸收指向性技能
+    Stoneskin_Totem = None # 石肤图腾, 为周围的团队成员增加护甲
+    Sentry_Totem = None # 哨兵图腾, 可以切换视野
+    Shamanistic_Rage = None # 萨满之怒, 天赋技能, 减伤并为普通攻击增加回蓝效果
+    Strength_of_Earth_Totem = None # 大地之力图腾, 为周围的团队成员增加力量敏捷
+    Water_Breathing = None # 水下呼吸
+    Water_Walking = None # 水上行走
+    Windfury_Totem = None # 风怒图腾, 为周围的团队成员增加近战急速
+    Windfury_Weapon = None # 风怒武器, 让你的攻击有可能触发连击效果
+    Wrath_of_Air_Totem = None # 空气之怒图腾, 为周围的团队成员增加施法急速
+
+
+class Restoration:
+    Ancestral_Spirit = None # 先祖灵魂, 复活
+    Chain_Heal = None # 治疗链
+    Cleanse_Spirit = None # 净化灵魂, 驱散 中毒, 疾病, 诅咒, 恢复系天赋技能
+    Cleansing_Totem = None # 净化图腾, 周期性为小队成员驱散 中毒, 疾病
+    Cure_Toxins = None # 驱毒术, 驱散 中毒, 疾病
+    Earth_Shield = None # 大地之盾, 当被保护的人受到伤害时治疗他, 恢复系天赋技能
+    Earthliving_Weapon = None # 大地生命武器,
+    Healing_Stream_Totem = None # 治疗之泉图腾
+    Healing_Wave = None # 治疗波
+    Lesser_Healing_Wave = None # 次级治疗波
+    Mana_Spring_Totem = None # 法力之泉图腾
+    Mana_Tide_Totem = None # 法力之潮图腾, 恢复系天赋技能
+    Nature_s_Swiftness = None # 自然迅捷, 使下一个自然法术瞬发, 恢复系天赋技能
+    Riptide = None # 激流, 恢复系天赋技能
+    Tidal_Force = None # 潮汐之力, 恢复系天赋技能
+    Totemic_Recall = None # 召回图腾
+    Tremor_Totem = None # 战栗图腾, 周期性为小队驱散恐惧
+    Water_Shield = None # 水之护盾
+
+
+
+class Healbot:
+    Ancestral_Spirit = None # 先祖灵魂, 复活
+    Chain_Heal = None # 治疗链
+    Cleanse_Spirit = None # 净化灵魂, 驱散 中毒, 疾病, 诅咒, 恢复系天赋技能
+    Cleansing_Totem = None # 净化图腾, 周期性为小队成员驱散 中毒, 疾病
+    Cure_Toxins = None # 驱毒术, 驱散 中毒, 疾病
+    Earth_Shield = None # 大地之盾, 当被保护的人受到伤害时治疗他, 恢复系天赋技能
+    Earthliving_Weapon = None # 大地生命武器,
+    Healing_Stream_Totem = None # 治疗之泉图腾
+    Healing_Wave = None # 治疗波
+    Lesser_Healing_Wave = None # 次级治疗波
+    Mana_Spring_Totem = None # 法力之泉图腾
+    Mana_Tide_Totem = None # 法力之潮图腾, 恢复系天赋技能
+    Nature_s_Swiftness = None # 自然迅捷, 使下一个自然法术瞬发, 恢复系天赋技能
+    Riptide = None # 激流, 恢复系天赋技能
+    Tidal_Force = None # 潮汐之力, 恢复系天赋技能
+    Totemic_Recall = None # 召回图腾
+    Tremor_Totem = None # 战栗图腾, 周期性为小队驱散恐惧
+    Water_Shield = None # 水之护盾
+
+
