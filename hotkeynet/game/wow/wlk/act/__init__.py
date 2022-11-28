64ad2ba9ee7b937ca11a62d355c282fc40863741
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-11-27 23:34:58 -0500

    Update __init__.py

--- hotkeynet/game/wow/wlk/act/__init__.py
+++ hotkeynet/game/wow/wlk/act/__init__.py
@@ -25,12 +25,31 @@ Hotkey 代码本身就能反映出想要实现的功能, 避免了写注释. 因
 修改. 这里的 ``hotkeynet.game.wow.wlk.act`` 模块中的代码请不要动!
 """
 
+from .common import (
+    movement,
+    pet_action,
+    target,
+    target_leader_key_mapper,
+    camera,
+    system,
+    general,
+)
 from .warrior import warrior, warrior_arm, warrior_fury, warrior_protection
 from .paladin import paladin, paladin_retribution, paladin_protection, paladin_holy
 from .dk import dk, dk_blood, dk_frost, dk_unholy
-from .shaman import shaman, shaman_elemental_combat, shaman_enhancement, shaman_restoration
+from .shaman import (
+    shaman,
+    shaman_elemental_combat,
+    shaman_enhancement,
+    shaman_restoration,
+)
 from .hunter import hunter, hunter_beastMastery, hunter_marksmanship, hunter_survival
 from .druid import druid, druid_balance, druid_restoration, druid_feral
-from .warlock import warlock, warlock_affliction, warlock_demonology, warlock_destruction
+from .warlock import (
+    warlock,
+    warlock_affliction,
+    warlock_demonology,
+    warlock_destruction,
+)
 from .mage import mage, mage_arcane, mage_fire, mage_frost
 from .priest import priest, priest_discipline, priest_holy, priest_shadow
