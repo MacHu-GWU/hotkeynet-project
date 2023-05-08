46318b37f56134ca4b5e0e5ab4f3248610db7834
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-05-08 09:51:07 -0400

    refactor

--- tests/test_script_hotkey.py
+++ tests/test_script_hotkey.py
@@ -29,28 +29,6 @@ class TestHotkey:
 
 
 if __name__ == "__main__":
-    import sys
-    import subprocess
+    from hotkeynet.tests import run_cov_test
 
-    abspath = os.path.abspath(__file__)
-    dir_project_root = os.path.dirname(abspath)
-    for _ in range(10):
-        if os.path.exists(os.path.join(dir_project_root, ".git")):
-            break
-        else:
-            dir_project_root = os.path.dirname(dir_project_root)
-    else:
-        raise FileNotFoundError("cannot find project root dir!")
-    dir_htmlcov = os.path.join(dir_project_root, "htmlcov")
-    bin_pytest = os.path.join(os.path.dirname(sys.executable), "pytest")
-
-    args = [
-        bin_pytest,
-        "-s", "--tb=native", "-vv",
-        f"--rootdir={dir_project_root}",
-        "--cov=hotkeynet.script",
-        "--cov-report", "term-missing",
-        "--cov-report", f"html:{dir_htmlcov}",
-        abspath,
-    ]
-    subprocess.run(args)
+    run_cov_test(__file__, "hotkeynet.script", preview=False)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,17 +10,9 @@ def test_render_template():
 
 
 def test_remove_empty_line():
-    content = (
-        "<div>\n"
-        "    \n"
-        "    <strong></strong>\n"
-        "    \n"
-        "</div>\n"
-    )
+    content = "<div>\n" "    \n" "    <strong></strong>\n" "    \n" "</div>\n"
     assert utils.remove_empty_line(content) == (
-        "<div>\n"
-        "    <strong></strong>\n"
-        "</div>"
+        "<div>\n" "    <strong></strong>\n" "</div>"
     )
 
 
@@ -41,29 +33,12 @@ def test_set_to_list():
     assert utils.set_to_list({3, 1, 2}, reverse=True) == [3, 2, 1]
 
 
+def test_has_duplicate():
+    assert utils.has_duplicate([1, 1, 2]) is True
+    assert utils.has_duplicate([1, 2, 3]) is False
+
+
 if __name__ == "__main__":
-    import sys
-    import subprocess
+    from hotkeynet.tests import run_cov_test
 
-    abspath = os.path.abspath(__file__)
-    dir_project_root = os.path.dirname(abspath)
-    for _ in range(10):
-        if os.path.exists(os.path.join(dir_project_root, ".git")):
-            break
-        else:
-            dir_project_root = os.path.dirname(dir_project_root)
-    else:
-        raise FileNotFoundError("cannot find project root dir!")
-    dir_htmlcov = os.path.join(dir_project_root, "htmlcov")
-    bin_pytest = os.path.join(os.path.dirname(sys.executable), "pytest")
-
-    args = [
-        bin_pytest,
-        "-s", "--tb=native",
-        f"--rootdir={dir_project_root}",
-        "--cov=hotkeynet.utils",
-        "--cov-report", "term-missing",
-        "--cov-report", f"html:{dir_htmlcov}",
-        abspath,
-    ]
-    subprocess.run(args)
+    run_cov_test(__file__, "hotkeynet.utils", preview=False)
